7d867694731ff4da377a5683a0e76fc0176e939c
shivam agarwal <shivamagarwaloff@gmail.com>
2022-04-25 02:38:05 +0530

    Sample Page

--- cryptodashboard/src/App.js
+++ cryptodashboard/src/App.js
@@ -1,16 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import NewsFeed from './Components/NewsFeed/index';
 function App() {
   return (
-    <>
-    <div className="middle">
-    
-        <img src="https://i.postimg.cc/bvMbsbwt/Bhai-Kya-Kar-Raha-Hai-Tu-Meme-Template-on-Ashneer-Grover-1024x614.jpg" />
-    </div>
-    <div className="middle">
-    <h1>Bhai Kya Kar rha hai tu ? Kaam Shuru Kar bhai!</h1>
-    </div>
+    <>  
+    <NewsFeed></NewsFeed>
   </>
   );
 }
